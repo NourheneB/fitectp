bd9496f4a3e00831425dc1ab25c9d46da14f25e2
Younes Dris <younes.dris@gmail.com>
2019-02-18 18:22:38 +0100

    Enumumération de jour

---
+++ src/ContosoUniversity/Enumeration/CourseDay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Enumeration
+{
+    public enum CourseDay
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+}
