86c301f35a8d2ee983b19284212cbf3f5209f7e8
Justyna Prive <jprive@talentsoft.com>
2019-02-20 14:16:21 +0100

    Creation AuthenticationBusinessTests

---
+++ tests/ContosoUniversity.Tests/Business/AuthenticationBusinessTests.cs
@@ -0,0 +1,24 @@
+﻿using ContosoUniversity.Controllers;
+using ContosoUniversity.Business;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using ContosoUniversity.Tests.Tools;
+using Moq;
+using NUnit.Framework;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System;
+
+namespace ContosoUniversity.Tests.Business
+{
+    [TestClass]
+    class AuthenticationBusinessTests : IntegrationTestsBase
+    {
+        [TestMethod]
+        public void CreateNewStudent_EtatInitial_EtatAttendu()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
