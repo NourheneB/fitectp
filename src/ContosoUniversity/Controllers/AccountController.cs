c5d4250b46fb27068acb3c52e0673dbad52d7b78
Nourhème Baouab <nbaouab@talentsoft.com>
2019-02-20 12:07:59 +0100

    Ajout Controlleur AccountControlleur

---
+++ src/ContosoUniversity/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using ContosoUniversity.Business;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(PersonVM model)
+
+        {
+            if(ModelState.IsValid)
+
+            {
+                if ( && model.Role == "Student")
+
+
+
+                    return View();
+        }
+
+    }
+}
