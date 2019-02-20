f43042fe9e500eda87d36f83d87d419d4bef140b
Nourhème Baouab <nbaouab@talentsoft.com>
2019-02-20 16:43:21 +0100

    Ajout Action Login dans AccountControlleur Ajout Méthode LoginPerson

---
+++ src/ContosoUniversity/ViewModels/LoginVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Login is required.")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Login must have 6 Characters")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Password is Required"), DataType(DataType.Password)]
+        [StringLength(64)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Role is required")]
+        public string Role { get; set; }
+    }
+}
