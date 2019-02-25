d2a92656a0f1ffa6f91da7dc3444c58167ba5254
Justyna Prive <jprive@talentsoft.com>
2019-02-25 09:49:12 +0100

    HashServiceTest

---
+++ tests/ContosoUniversity.Tests/Controllers/UsernameApiControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using ContosoUniversity.Tests.Tools;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Tests.Controllers
+{
+    class UsernameApiControllerTests : IntegrationTestsBase
+    {
+        [Test]
+        public void Get_username_OkTrue()
+        {
+
+        }
+    }
+}
---
+++ tests/ContosoUniversity.Tests/Services/HashServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using ContosoUniversity.Services;
+using ContosoUniversity.Tests.Tools;
+
+namespace ContosoUniversity.Tests.Services
+{
+    class HashServiceTests : IntegrationTestsBase
+    {
+
+        [Test]
+        public void GenerateSHA256String_stringToCompare_True()
+        {
+
+            string inputString = "password123";
+            string hash = HashService.GenerateSHA256String(inputString);
+            string stringToCompare = HashService.GenerateSHA256String("password123");
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            int result = comparer.Compare(stringToCompare, hash);
+
+            Assert.AreEqual(0, result);
+
+        }
+    }
+}
